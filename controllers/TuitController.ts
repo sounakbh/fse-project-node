929bbbd780336e604305a76bcdb8aef5d8c97180
sounakbh <sounakbh@gmail.com>
2022-04-23 14:39:42 -0400

    find all tuits with tag name

--- controllers/TuitController.ts
+++ controllers/TuitController.ts
@@ -3,12 +3,9 @@
  */
 import TuitDao from "../daos/TuitDao";
 import Tuit from "../models/tuits/Tuit";
-import {Express, Request, Response} from "express";
+import { Express, Request, Response } from "express";
 import TuitControllerI from "../interfaces/TuitControllerI";
 import TagDao from "../daos/TagDao";
-import Tag from "../models/tags/Tag";
-import {compileFunction} from "vm";
-import {promises} from "dns";
 
 /**
  * @class TuitController Implements RESTful Web service API for tuits resource.
@@ -27,129 +24,159 @@ import {promises} from "dns";
  * RESTful Web service API
  */
 export default class TuitController implements TuitControllerI {
-    private static tuitDao: TuitDao = TuitDao.getInstance();
-    private static tagDao: TagDao = TagDao.getInstance();
-    private static tuitController: TuitController | null = null;
+  private static tuitDao: TuitDao = TuitDao.getInstance();
+  private static tagDao: TagDao = TagDao.getInstance();
+  private static tuitController: TuitController | null = null;
 
-    /**
-     * Creates singleton controller instance
-     * @param {Express} app Express instance to declare the RESTful Web service
-     * API
-     * @return TuitController
-     */
-    public static getInstance = (app: Express): TuitController => {
-        if(TuitController.tuitController === null) {
-            TuitController.tuitController = new TuitController();
-            app.get("/api/tuits", TuitController.tuitController.findAllTuits);
-            app.get("/api/users/:uid/tuits", TuitController.tuitController.findAllTuitsByUser);
-            app.get("/api/tuits/:uid", TuitController.tuitController.findTuitById);
-            app.post("/api/users/:uid/tuits", TuitController.tuitController.createTuitByUser);
-            app.put("/api/tuits/:uid", TuitController.tuitController.updateTuit);
-            app.delete("/api/tuits/:uid", TuitController.tuitController.deleteTuit);
-        }
-        return TuitController.tuitController;
+  /**
+   * Creates singleton controller instance
+   * @param {Express} app Express instance to declare the RESTful Web service
+   * API
+   * @return TuitController
+   */
+  public static getInstance = (app: Express): TuitController => {
+    if (TuitController.tuitController === null) {
+      TuitController.tuitController = new TuitController();
+      app.get("/api/tuits", TuitController.tuitController.findAllTuits);
+      app.get(
+        "/api/users/:uid/tuits",
+        TuitController.tuitController.findAllTuitsByUser
+      );
+      app.get("/api/tuits/:uid", TuitController.tuitController.findTuitById);
+      app.get(
+        "/api/tuits/tags/:tagName",
+        TuitController.tuitController.findAllTuitsByTagID
+      );
+      app.post(
+        "/api/users/:uid/tuits",
+        TuitController.tuitController.createTuitByUser
+      );
+      app.put("/api/tuits/:uid", TuitController.tuitController.updateTuit);
+      app.delete("/api/tuits/:uid", TuitController.tuitController.deleteTuit);
     }
+    return TuitController.tuitController;
+  };
 
-    private constructor() {}
+  private constructor() {}
 
-    /**
-     * Retrieves all tuits from the database and returns an array of tuits.
-     * @param {Request} req Represents request from client
-     * @param {Response} res Represents response to client, including the
-     * body formatted as JSON arrays containing the tuit objects
-     */
-    findAllTuits = (req: Request, res: Response) =>
-        TuitController.tuitDao.findAllTuits()
-            .then((tuits: Tuit[]) => res.json(tuits));
-    
-    /**
-     * @param {Request} req Represents request from client, including path
-     * parameter tid identifying the primary key of the tuit to be retrieved
-     * @param {Response} res Represents response to client, including the
-     * body formatted as JSON containing the tuit that matches the user ID
-     */
-    findTuitById = (req: Request, res: Response) =>
-        TuitController.tuitDao.findTuitById(req.params.uid)
-            .then((tuit: Tuit) => res.json(tuit));
+  /**
+   * Retrieves all tuits from the database and returns an array of tuits.
+   * @param {Request} req Represents request from client
+   * @param {Response} res Represents response to client, including the
+   * body formatted as JSON arrays containing the tuit objects
+   */
+  findAllTuits = (req: Request, res: Response) =>
+    TuitController.tuitDao
+      .findAllTuits()
+      .then((tuits: Tuit[]) => res.json(tuits));
 
-    /**
-     * Retrieves all tuits from the database for a particular user and returns
-     * an array of tuits.
-     * @param {Request} req Represents request from client
-     * @param {Response} res Represents response to client, including the
-     * body formatted as JSON arrays containing the tuit objects
-     */
-    findAllTuitsByUser = (req: Request, res: Response) => {
-        // @ts-ignore
-        let userId = req.params.uid === "my" && req.session['profile'] ?
-            // @ts-ignore
-            req.session['profile']._id : req.params.uid;
-        if (userId === "my") {
-            res.sendStatus(503);
-            return;
-        }
-        TuitController.tuitDao.findAllTuitsByUser(userId)
-            .then((tuits: Tuit[]) => res.json(tuits));
+  /**
+   * @param {Request} req Represents request from client, including path
+   * parameter tid identifying the primary key of the tuit to be retrieved
+   * @param {Response} res Represents response to client, including the
+   * body formatted as JSON containing the tuit that matches the user ID
+   */
+  findTuitById = (req: Request, res: Response) =>
+    TuitController.tuitDao
+      .findTuitById(req.params.uid)
+      .then((tuit: Tuit) => res.json(tuit));
+
+  /**
+   * Retrieves all tuits from the database for a particular user and returns
+   * an array of tuits.
+   * @param {Request} req Represents request from client
+   * @param {Response} res Represents response to client, including the
+   * body formatted as JSON arrays containing the tuit objects
+   */
+  findAllTuitsByUser = (req: Request, res: Response) => {
+    let userId =
+      // @ts-ignore
+      req.params.uid === "my" && req.session["profile"]
+        ? // @ts-ignore
+          req.session["profile"]._id
+        : req.params.uid;
+    if (userId === "my") {
+      res.sendStatus(503);
+      return;
     }
+    TuitController.tuitDao
+      .findAllTuitsByUser(userId)
+      .then((tuits: Tuit[]) => res.json(tuits));
+  };
 
-
-    /**
-     * @param {Request} req Represents request from client, including body
-     * containing the JSON object for the new tuit to be inserted in the
-     * database
-     * @param {Response} res Represents response to client, including the
-     * body formatted as JSON containing the new tuit that was inserted in the
-     * database
-     */
-    createTuitByUser = async (req: Request, res: Response) => {
-        // @ts-ignore
-        let userId = req.params.uid === "my" && req.session['profile'] ?
-            // @ts-ignore
-            req.session['profile']._id : req.params.uid;
-        if (userId === "my") {
-            res.sendStatus(503);
-            return;
-        }
-        let tags = req.body.tags;
-        const tagDao = TuitController.tagDao
-        let tagIds: any[] = []
-
-        for (let tag of tags) {
-            const isTagPresent = await tagDao.findTagByName(tag);
-            if(isTagPresent) {
-                const frequency = isTagPresent.frequency + 1;
-                await tagDao.updateTagStats(isTagPresent._id, frequency);
-                tagIds.push(isTagPresent._id);
-            }
-            else {
-                await tagDao.createNewTag(tag);
-                const id = await tagDao.findTagIdByTagName(tag);
-                tagIds.push(id[0]._id)
-            }
-        }
-        req.body.tags = tagIds
-        TuitController.tuitDao.createTuitByUser(userId, req.body)
-            .then((tuit: Tuit) => res.json(tuit));
-        return res.status(200);
+  /**
+   * @param {Request} req Represents request from client, including body
+   * containing the JSON object for the new tuit to be inserted in the
+   * database
+   * @param {Response} res Represents response to client, including the
+   * body formatted as JSON containing the new tuit that was inserted in the
+   * database
+   */
+  createTuitByUser = async (req: Request, res: Response) => {
+    let userId =
+      // @ts-ignore
+      req.params.uid === "my" && req.session["profile"]
+        ? // @ts-ignore
+          req.session["profile"]._id
+        : req.params.uid;
+    if (userId === "my") {
+      res.sendStatus(503);
+      return;
     }
+    let tags = req.body.tags;
+    const tagDao = TuitController.tagDao;
+    let tagIds: any[] = [];
 
-    /**
-     * @param {Request} req Represents request from client, including path
-     * parameter tid identifying the primary key of the tuit to be modified
-     * @param {Response} res Represents response to client, including status
-     * on whether updating a tuit was successful or not
-     */
-    updateTuit = (req: Request, res: Response) =>
-        TuitController.tuitDao.updateTuit(req.params.uid, req.body)
-            .then((status) => res.send(status));
+    for (let tag of tags) {
+      const isTagPresent = await tagDao.findTagByName(tag);
+      if (isTagPresent) {
+        const frequency = isTagPresent.frequency + 1;
+        await tagDao.updateTagStats(isTagPresent._id, frequency);
+        tagIds.push(isTagPresent._id);
+      } else {
+        await tagDao.createNewTag(tag);
+        const id = await tagDao.findTagIdByTagName(tag);
+        tagIds.push(id[0]._id);
+      }
+    }
+    req.body.tags = tagIds;
+    TuitController.tuitDao
+      .createTuitByUser(userId, req.body)
+      .then((tuit: Tuit) => res.json(tuit));
+    return res.status(200);
+  };
 
-    /**
-     * @param {Request} req Represents request from client, including path
-     * parameter tid identifying the primary key of the tuit to be removed
-     * @param {Response} res Represents response to client, including status
-     * on whether deleting a user was successful or not
-     */
-    deleteTuit = (req: Request, res: Response) =>
-        TuitController.tuitDao.deleteTuit(req.params.uid)
-            .then((status) => res.send(status));
-};
+  /**
+   * @param {Request} req Represents request from client, including path
+   * parameter tid identifying the primary key of the tuit to be modified
+   * @param {Response} res Represents response to client, including status
+   * on whether updating a tuit was successful or not
+   */
+  updateTuit = (req: Request, res: Response) =>
+    TuitController.tuitDao
+      .updateTuit(req.params.uid, req.body)
+      .then((status) => res.send(status));
+
+  /**
+   * @param {Request} req Represents request from client, including path
+   * parameter tid identifying the primary key of the tuit to be removed
+   * @param {Response} res Represents response to client, including status
+   * on whether deleting a user was successful or not
+   */
+  deleteTuit = (req: Request, res: Response) =>
+    TuitController.tuitDao
+      .deleteTuit(req.params.uid)
+      .then((status) => res.send(status));
+
+  findAllTuitsByTagID = async (req: Request, res: Response) => {
+    const tagName = req.params.tagName;
+    // get Tag ID
+    const tagIDResponse = await TuitController.tagDao.findTagIdByTagName(
+      tagName
+    );
+    const tagID = tagIDResponse[0]._id.toString();
+    return TuitController.tuitDao
+      .findAllTuitsByTagID(tagID)
+      .then((status) => res.send(status));
+  };
+}
--- daos/TuitDao.ts
+++ daos/TuitDao.ts
@@ -11,43 +11,37 @@ import TuitDaoI from "../interfaces/TuitDaoI";
  * of Users
  * @property {UserDao} userDao Private single instance of UserDao
  */
-export default class TuitDao implements TuitDaoI{
-    private static tuitDao: TuitDao | null = null;
-    public static getInstance = (): TuitDao => {
-        if(TuitDao.tuitDao === null) {
-            TuitDao.tuitDao = new TuitDao();
-        }
-        return TuitDao.tuitDao;
+export default class TuitDao implements TuitDaoI {
+  private static tuitDao: TuitDao | null = null;
+  public static getInstance = (): TuitDao => {
+    if (TuitDao.tuitDao === null) {
+      TuitDao.tuitDao = new TuitDao();
     }
-    private constructor() {}
-    findAllTuits = async (): Promise<Tuit[]> =>
-        TuitModel.find()
-            .populate("postedBy")
-            .exec();
-    findAllTuitsByUser = async (uid: string): Promise<Tuit[]> =>
-        TuitModel.find({postedBy: uid})
-            .sort({'postedOn': -1})
-            .populate("postedBy")
-            .exec();
-    findTuitById = async (uid: string): Promise<any> =>
-        TuitModel.findById(uid)
-            .populate("postedBy")
-            .exec();
-    createTuitByUser = async (uid: string, tuit: Tuit): Promise<Tuit> =>
-        TuitModel.create({...tuit, postedBy: uid});
-    updateTuit = async (tid: string, tuit: Tuit): Promise<any> =>
-        TuitModel.updateOne(
-            {_id: tid},
-            {$set: tuit});
-    updateLikes = async (tid: string, newStats: any): Promise<any> =>
-        TuitModel.updateOne(
-            {_id: tid},
-            {$set: {stats: newStats}}
-        );
-    deleteTuit = async (uid: string): Promise<any> =>
-        TuitModel.deleteOne({_id: uid});
+    return TuitDao.tuitDao;
+  };
+  private constructor() {}
+  findAllTuits = async (): Promise<Tuit[]> =>
+    TuitModel.find().populate("postedBy").exec();
+  findAllTuitsByUser = async (uid: string): Promise<Tuit[]> =>
+    TuitModel.find({ postedBy: uid })
+      .sort({ postedOn: -1 })
+      .populate("postedBy")
+      .exec();
+  findTuitById = async (uid: string): Promise<any> =>
+    TuitModel.findById(uid).populate("postedBy").exec();
+  createTuitByUser = async (uid: string, tuit: Tuit): Promise<Tuit> =>
+    TuitModel.create({ ...tuit, postedBy: uid });
+  updateTuit = async (tid: string, tuit: Tuit): Promise<any> =>
+    TuitModel.updateOne({ _id: tid }, { $set: tuit });
+  updateLikes = async (tid: string, newStats: any): Promise<any> =>
+    TuitModel.updateOne({ _id: tid }, { $set: { stats: newStats } });
+  deleteTuit = async (uid: string): Promise<any> =>
+    TuitModel.deleteOne({ _id: uid });
 
-    findAllTuitsByTagID = async (tagid: string): Promise<Tuit[]> =>
-        TuitModel.find({tags: tagid});
-
+  findAllTuitsByTagID = async (tagid: string): Promise<Tuit[]> =>
+    TuitModel.find({
+      tags: {
+        $all: [tagid],
+      },
+    });
 }
